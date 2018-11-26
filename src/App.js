8144c632afd0115a5c372778f585ace7f6c7eb72
dkotsuka <dkotsuka@gmail.com>
2018-11-25 22:51:10 -0200

    feat: dynamic book list loading
    Book lists are loading and rendering dynamically by react components.
    To fix: BookList Components re-rendering doesn't working after state change.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+import BookList from './BookList'
 
-class App extends Component {
+class BooksApp extends React.Component {
+  state = {
+    currentlyReading: [],
+    wantToRead: [],
+    read: [],
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then(books => books.map((book) => {
+      this.setState(state => state[book.shelf].push(book))
+    }))
+  }
+
+  changeShelf = (book, shelf) => {
+    BooksAPI.update(book,shelf).then(() => {
+        this.setState(state => {state[book.shelf].filter((b) => b.id !== book.id)})
+        this.setState(state => {state[shelf].push(book)})
+    })
+  }
+
   render() {
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        {this.state.showSearchPage ? (
+          <div className="search-books">
+            <div className="search-books-bar">
+              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author"/>
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+        ) : (
+          <div className="list-books">
+
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+
+            <div className="list-books-content">
+              <div>
+                <BookList books={this.state.currentlyReading} 
+                  shelfTitle='Currently Reading' 
+                  onChangeShelf={this.changeShelf}/>
+
+                <BookList books={this.state.wantToRead} 
+                  shelfTitle='Want to Read' 
+                  onChangeShelf={this.changeShelf}/>
+
+                <BookList books={this.state.read} 
+                  shelfTitle='Read' 
+                  onChangeShelf={this.changeShelf}/>
+              </div>
+            </div>
+
+            <div className="open-search">
+              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+            </div>
+          </div>
+        )}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default BooksApp
---
+++ src/BookList.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function BookList(props) {
+	return (
+		<div className="bookshelf">
+	        <h2 className="bookshelf-title">{props.shelfTitle}</h2>
+	        <div className="bookshelf-books">
+				<ol className="books-grid">
+					{props.books.map((book, index) => (
+						<li key={book.id}>
+			                <div className="book">
+								<div className="book-top">
+									<div className="book-cover" style={
+										{ 
+											width: 128, height: 193, 
+											backgroundImage: `url(${book.imageLinks.thumbnail})`
+										}
+									}></div>
+									<div className="book-shelf-changer">
+										<select value={book.shelf} onChange={(event) => props.onChangeShelf(book, event.target.value)}>
+										    <option value="move" disabled>Move to...</option>
+										    <option value="currentlyReading">Currently Reading</option>
+										    <option value="wantToRead">Want to Read</option>
+										    <option value="read">Read</option>
+										    <option value="none">None</option>
+										</select>
+									</div>
+								</div>
+			                	<div className="book-title">{book.title}</div>
+			                	<div className="book-authors">{book.authors}</div>
+			                </div>
+			            </li>
+					))}
+	            </ol>
+			</div>
+		</div>
+	)
+}
+
+export default BookList
